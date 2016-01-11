aa19fb568644164703e9895ae910cfce78f36716
JamiePaton <jamieep@gmail.com>
2016-01-11 20:44:46 +0000

    added Item class

--- tracker.py
+++ tracker.py
@@ -5,13 +5,18 @@ Created on Mon Jan 11 00:37:07 2016
 @author: jamie
 """
 
-TITLE = 'tracker'
-VERSION = '0.0.1'
-AUTHOR = 'Jamie Paton'
 import jsonobject
+import jsonpickle
 import logging
 import string
 import random
+import uuid
+
+
+TITLE = 'tracker'
+VERSION = '0.0.2'
+AUTHOR = 'Jamie Paton'
+
 
 characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 characters = characters.replace("I", "")
@@ -21,5 +26,23 @@ print characters
 print "{}{}".format(random.choice(string.ascii_uppercase), random.choice(string.ascii_uppercase))
 
 
+
+class Item(jsonobject.JSONObject):
+    def __init__(self, name, stored=False, location=None):
+        self.name = name
+        self.stored = stored
+        self.location = location
+        self.id = uuid.uuid4()
+
+def add_item(item):
+    items.append(item)
+    save()
+
+def save():
+    with open('items.json', 'w') as f:
+        f.write(jsonpickle.encode(items))
+
 if __name__ == '__main__':
     print ''.join([TITLE, ' v', VERSION, ' ', AUTHOR])
+    items = []
+    add_item(Item("fish"))
