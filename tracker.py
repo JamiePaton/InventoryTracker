8d748c5e8543f48395b73db566c95d2b63c22d42
JamiePaton <jamieep@gmail.com>
2016-01-11 00:38:58 +0000

    created main files

---
+++ tracker.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 00:37:07 2016
+
+@author: jamie
+"""
+
+TITLE = 'tracker'
+VERSION = '0.0.1'
+AUTHOR = 'Jamie Paton'
+import jsonobject
+import logging
+
+if __name__ == '__main__':
+    print ''.join([TITLE, ' v', VERSION, ' ', AUTHOR])
